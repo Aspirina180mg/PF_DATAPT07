d27122405cf3aa083d44fb5759167ebd3da8df00
Misael García Torres <misagtor@gmail.com>
2024-06-06 01:07:21 -0400

    update streamlit

--- main.py
+++ main.py
@@ -37,17 +37,6 @@ def get_atributos(id_restaurante):
         atributos_dict[ta][a] = df.loc[:, f"{ta}_{a}"].values[0]
     return atributos_dict
 
-# Dividir el espacio horizontalmente
-col1, col2 = st.columns([1, 3])
-
-# Dropdown con los id_restaurante
-id_restaurantes = X_subway['id_restaurante'].tolist()
-selected_id = col1.selectbox("Seleccionar ID de Restaurante", id_restaurantes)
-
-# Generar enlace con el formato especificado
-enlace = f"https://ptf-data-subway.streamlit.app/?id={selected_id}"
-col2.write("Enlace:", enlace)
-
 # Dropdown con los id_restaurante
 id_restaurantes = X_subway['id_restaurante'].tolist()
 selected_id = st.selectbox("Seleccionar ID de Restaurante", id_restaurantes)
