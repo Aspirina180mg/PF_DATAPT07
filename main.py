6be8acc0c5b2261aee9cc38e0456d782725fbd92
Misael García Torres <misagtor@gmail.com>
2024-06-06 00:35:24 -0400

    update streamlit y caché

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pickle
 st.set_page_config(layout="wide")
 st.title("Restaurantes y Caraterísticas")
 
-
 # Datos proporcionados
 @st.cache_data
 def read_data():
@@ -15,9 +14,7 @@ def read_data():
     modelo = pickle.load(open(filename, "rb"))
     return X_subway, X_subway_proc, modelo
 
-
 X_subway, X_subway_proc, modelo = read_data()
-
 
 def get_atributos(id_restaurante):
     df = X_subway.query(f"id_restaurante == '{id_restaurante}'")
@@ -40,14 +37,26 @@ def get_atributos(id_restaurante):
         atributos_dict[ta][a] = df.loc[:, f"{ta}_{a}"].values[0]
     return atributos_dict
 
+@st.cache
+def cargar_datos():
+    X_subway = pd.read_parquet("ML/X_subway.parquet")
+    return X_subway
+
+X_subway = cargar_datos()
+
+# Definir función para generar enlace
+@st.cache
+def generar_enlace(selected_id):
+    return f"https://ptf-data-subway.streamlit.app/?id={selected_id}"
 
 # Dropdown con los id_restaurante
-id_restaurantes = X_subway["id_restaurante"].tolist()
+id_restaurantes = X_subway['id_restaurante'].tolist()
 selected_id = st.selectbox("Seleccionar ID de Restaurante", id_restaurantes)
 
-# Generar enlace con el formato especificado
-enlace = f"https://ptf-data-subway.streamlit.app/?id={selected_id}"
+# Generar enlace solo si se modifica el dropdown
+enlace = generar_enlace(selected_id)
 
+# Mostrar enlace
 st.write("Enlace:", enlace)
 
 muestra = X_subway.query(f"id_restaurante == '{st.query_params['id']}'")
@@ -78,16 +87,286 @@ nombres_atributo = {
 
 with st.form("atributos_form"):
     submited_data = {}
-    tabs = [st.container() for _ in nombres_atributo.values()]
-    for i, (atributo, nombre) in enumerate(nombres_atributo.items()):
-        with tabs[i]:
-            st.write(nombre)
-            for tipo_atributo, value in data_2[atributo].items():
-                check_key = f"{atributo}_{tipo_atributo}"
-                is_checked = st.checkbox(
-                    tipo_atributo, value=bool(value), key=check_key
-                )
-                submited_data[check_key] = int(is_checked)
+    (
+        t_access,
+        t_amen,
+        t_atmos,
+        t_crowd,
+        t_dining,
+        t_health,
+        t_high,
+        t_offer,
+        t_pay,
+        t_popular,
+        t_service,
+    ) = st.tabs([i for i in nombres_atributo.values()])
+
+    with t_access:
+        c_access = st.container()
+
+        with c_access:
+            prefix = "access"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo[prefix]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    "Wheelchair accessible entrance"
+                    "Wheelchair accessible parking lot"
+                    "Wheelchair accessible restroom"
+                    "Wheelchair accessible seating"
+                    "Wheelchair accessible elevator"
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+
+    with t_amen:
+        c_amen = st.container()
+
+        with c_amen:
+            prefix = "amen"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo["amen"]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    "Bar on site"
+                    "Gender-neutral restroom"
+                    "Good for kids"
+                    "High chairs"
+                    "Restroom"
+                    "Wi-Fi"
+
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+
+    with t_atmos:
+        c_atmos = st.container()
+        with c_atmos:
+            prefix = "atmos"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo[prefix]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    for tipo_atributo in data_2[prefix].items():
+                        st.write(f"{tipo_atributo[0]}")
+
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+
+    with t_crowd:
+        c_crowd = st.container()
+        with c_crowd:
+            prefix = "crowd"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo[prefix]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    for tipo_atributo in data_2[prefix].items():
+                        st.write(f"{tipo_atributo[0]}")
+
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+    with t_dining:
+        c_dining = st.container()
+        with c_dining:
+            prefix = "dining"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo[prefix]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    for tipo_atributo in data_2[prefix].items():
+                        st.write(f"{tipo_atributo[0]}")
+
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+    with t_health:
+        c_health = st.container()
+        with c_health:
+            prefix = "health"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo[prefix]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    for tipo_atributo in data_2[prefix].items():
+                        st.write(f"{tipo_atributo[0]}")
+
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+    with t_high:
+        c_high = st.container()
+        with c_high:
+            prefix = "high"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo[prefix]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    for tipo_atributo in data_2[prefix].items():
+                        st.write(f"{tipo_atributo[0]}")
+
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+    with t_offer:
+        c_offer = st.container()
+        with c_offer:
+            prefix = "offer"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo[prefix]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    for tipo_atributo in data_2[prefix].items():
+                        st.write(f"{tipo_atributo[0]}")
+
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+
+    with t_pay:
+        c_pay = st.container()
+        with c_pay:
+            prefix = "pay"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo[prefix]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    for tipo_atributo in data_2[prefix].items():
+                        st.write(f"{tipo_atributo[0]}")
+
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+    with t_popular:
+        c_popular = st.container()
+        with c_popular:
+            prefix = "popular"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo[prefix]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    for tipo_atributo in data_2[prefix].items():
+                        st.write(f"{tipo_atributo[0]}")
+
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+    with t_service:
+        c_service = st.container()
+        with c_service:
+            prefix = "service"
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                nombres_atributo[prefix]
+            with col2:
+                col_2a, col_2b = st.columns(2)
+                with col_2a:
+                    for tipo_atributo in data_2[prefix].items():
+                        st.write(f"{tipo_atributo[0]}")
+
+                with col_2b:
+                    for tipo_atributo in data_2[prefix].items():
+                        check_key = f"{prefix}_{tipo_atributo[0]}"
+                        is_checked = col_2b.checkbox(
+                            "",
+                            key=check_key,
+                            value=bool(data_2[prefix][tipo_atributo[0]]),
+                        )
+                        submited_data[check_key] = int(is_checked)
+
     submitted = st.form_submit_button("Evaluar")
 
     if submitted:
